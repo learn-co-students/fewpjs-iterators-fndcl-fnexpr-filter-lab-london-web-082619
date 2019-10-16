c9d4abc5183e6ac15c6819677f30dcb53c2992ec
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-10-16 10:44:41 +0100

    Done.

--- index.js
+++ index.js
@@ -24,3 +24,29 @@ function findMatching(drivers, string){
     return matchingDrivers;
 }
 
+function fuzzyMatch(drivers, letters){
+    let fuzzyMatches = [];
+
+    function fuzzy(driver, letters){
+        if (driver.match(`^${letters}`)){
+            fuzzyMatches.push(driver)
+        }
+    }
+
+    drivers.filter(driver => fuzzy(driver, letters));
+    return fuzzyMatches;
+}
+
+function matchName(driversObjects, string){
+
+    let matchingDrivers = [];
+
+    function checkName(driverObject, string){
+        if (driverObject.name.toLowerCase() == string.toLowerCase()){
+            matchingDrivers.push(driverObject);
+        }
+    }
+
+    driversObjects.filter(driverObject => checkName(driverObject, string));
+    return matchingDrivers;
+}
