9076ae618962d5d03565e14d7408d582023f57d3
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-10-16 10:33:28 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+
+const body = document.querySelector('body')
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Hello');
+    findMatching(drivers, 'Sally');
+
+})
+
+function findMatching(drivers, string){
+
+    let matchingDrivers = [];
+
+    function checkName(name, string){
+        if (name.toLowerCase() == string.toLowerCase()){
+            matchingDrivers.push(name);
+        }
+    }
+
+    return drivers.filter(driver => checkName(driver, string));
+    debugger
+}
+
